ece11175868d89d3e47d8f225ef8203f50dd9f3c
Xehtel <xtl.social@protonmail.com>
2026-06-08 14:20:16 +0000

    Update hdc3022_Sensor.py

--- HDC302x/hdc3022_Sensor.py
+++ HDC302x/hdc3022_Sensor.py
@@ -42,39 +42,39 @@ class HDC3022_Sensor:
     #Grabs Data from Each Sensor & Sends it to Corresponding Data Lists
     def grab_data():
         #Clear the Lists for Next Iteration of Data
-        HDC302X_Sensors.timestamps.clear()
-        HDC302X_Sensors.nameList.clear()
-        HDC302X_Sensors.addrsList.clear()
-        HDC302X_Sensors.tempsList.clear()
-        HDC302X_Sensors.humsList.clear()
+        HDC3022_Sensor.timestamps.clear()
+        HDC3022_Sensor.nameList.clear()
+        HDC3022_Sensor.addrsList.clear()
+        HDC3022_Sensor.tempsList.clear()
+        HDC3022_Sensor.humsList.clear()
         #Loops Through an Iteration of Data
-        for i, (sensor_name, addrs, sensor) in enumerate(HDC302X_Sensor.sensorList):
+        for i, (sensor_name, addrs, sensor) in enumerate(HDC3022_Sensor.sensorList):
             #Try to Grab Sensor Data, Fill with Placeholders if no Sensor
             try:
                 #Grabs Sensor Data
                 temp = sensor.temperature
                 hum = sensor.relative_humidity
                 #Appends Sensor Data
-                HDC302X_Sensors.nameList.append(sensor_name)
-                HDC302X_Sensors.addrsList.append(addrs)
-                HDC302X_Sensors.tempsList.append(temp)
-                HDC302X_Sensors.humsList.append(hum)
+                HDC3022_Sensor.nameList.append(sensor_name)
+                HDC3022_Sensor.addrsList.append(addrs)
+                HDC3022_Sensor.tempsList.append(temp)
+                HDC3022_Sensor.humsList.append(hum)
                 #Takes Current Timestamp
                 now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
                 times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
                 #Appends Timestamp
-                HDC302X_Sensors.timestamps.append(times)
+                HDC3022_Sensor.timestamps.append(times)
             except Exception as e:
                 #Appends Placeholder Sensor Data
-                HDC302X_Sensors.nameList.append(sensor_name)
-                HDC302X_Sensors.addrsList.append(addrs)
-                HDC302X_Sensors.tempsList.append(0.0)
-                HDC302X_Sensors.humsList.append(0.0)
+                HDC3022_Sensor.nameList.append(sensor_name)
+                HDC3022_Sensor.addrsList.append(addrs)
+                HDC3022_Sensor.tempsList.append(0.0)
+                HDC3022_Sensor.humsList.append(0.0)
                 #Takes Current Timestamp
                 now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
                 times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
                 #Appends Timestamp
-                HDC302X_Sensors.timestamps.append(times)
+                HDC3022_Sensor.timestamps.append(times)
 #=====================================END OF MAIN FUNCTIONS======================================#
 
     
@@ -88,14 +88,14 @@ class HDC3022_Sensor:
         #Debug Prints for Bus 0 & Bus 1 (All Sensors on sensorList) 
         print(f"\n================================SENSOR LOGGER================================")
         print(f"---BUS 0---")
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[0], HDC302X_Sensor.addrsList[0], HDC302X_Sensor.tempsList[0], HDC302X_Sensor.humsList[0], HDC302X_Sensor.timestamps[0])) #Bus 0 Sensor 1 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[1], HDC302X_Sensor.addrsList[1], HDC302X_Sensor.tempsList[1], HDC302X_Sensor.humsList[1], HDC302X_Sensor.timestamps[1])) #Bus 0 Sensor 2 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[2], HDC302X_Sensor.addrsList[2], HDC302X_Sensor.tempsList[2], HDC302X_Sensor.humsList[2], HDC302X_Sensor.timestamps[2])) #Bus 0 Sensor 3 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[3], HDC302X_Sensor.addrsList[3], HDC302X_Sensor.tempsList[3], HDC302X_Sensor.humsList[3], HDC302X_Sensor.timestamps[3])) #Bus 0 Sensor 4 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[0], HDC3022_Sensor.addrsList[0], HDC3022_Sensor.tempsList[0], HDC3022_Sensor.humsList[0], HDC3022_Sensor.timestamps[0])) #Bus 0 Sensor 1 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[1], HDC3022_Sensor.addrsList[1], HDC3022_Sensor.tempsList[1], HDC3022_Sensor.humsList[1], HDC3022_Sensor.timestamps[1])) #Bus 0 Sensor 2 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[2], HDC3022_Sensor.addrsList[2], HDC3022_Sensor.tempsList[2], HDC3022_Sensor.humsList[2], HDC3022_Sensor.timestamps[2])) #Bus 0 Sensor 3 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[3], HDC3022_Sensor.addrsList[3], HDC3022_Sensor.tempsList[3], HDC3022_Sensor.humsList[3], HDC3022_Sensor.timestamps[3])) #Bus 0 Sensor 4 Log
         print(f"---BUS 1---")
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[4], HDC302X_Sensor.addrsList[4], HDC302X_Sensor.tempsList[4], HDC302X_Sensor.humsList[4], HDC302X_Sensor.timestamps[4])) #Bus 1 Sensor 5 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[5], HDC302X_Sensor.addrsList[5], HDC302X_Sensor.tempsList[5], HDC302X_Sensor.humsList[5], HDC302X_Sensor.timestamps[5])) #Bus 1 Sensor 6 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[6], HDC302X_Sensor.addrsList[6], HDC302X_Sensor.tempsList[6], HDC302X_Sensor.humsList[6], HDC302X_Sensor.timestamps[6])) #Bus 1 Sensor 7 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC302X_Sensor.nameList[7], HDC302X_Sensor.addrsList[7], HDC302X_Sensor.tempsList[7], HDC302X_Sensor.humsList[7], HDC302X_Sensor.timestamps[7])) #Bus 1 Sensor 8 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[4], HDC3022_Sensor.addrsList[4], HDC3022_Sensor.tempsList[4], HDC3022_Sensor.humsList[4], HDC3022_Sensor.timestamps[4])) #Bus 1 Sensor 5 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[5], HDC3022_Sensor.addrsList[5], HDC3022_Sensor.tempsList[5], HDC3022_Sensor.humsList[5], HDC3022_Sensor.timestamps[5])) #Bus 1 Sensor 6 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[6], HDC3022_Sensor.addrsList[6], HDC3022_Sensor.tempsList[6], HDC3022_Sensor.humsList[6], HDC3022_Sensor.timestamps[6])) #Bus 1 Sensor 7 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(HDC3022_Sensor.nameList[7], HDC3022_Sensor.addrsList[7], HDC3022_Sensor.tempsList[7], HDC3022_Sensor.humsList[7], HDC3022_Sensor.timestamps[7])) #Bus 1 Sensor 8 Log
         time.sleep(1.0) #Functions Print Delay in Seconds
 #===================================END OF DEBUGGING FUNCTIONS===================================#
